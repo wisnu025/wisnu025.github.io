f9d97573a4c23354cd41a0b52db2ab78e8fc3808
wisnu025 <wisnu.pratama.ariyoga@gmail.com>
2024-08-20 16:17:46 +0700

    Add files via upload
    update

---
+++ javascript/script.js
@@ -0,0 +1,51 @@
+// window.addEventListener ("scroll",function() {
+// let header = document.querySelector(".header");
+// header.classList.toggle("sticky", window.scrollY > 0);
+// });
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
+
+window.onscroll=()=> {
+    sections.forEach(sec =>
+    {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLink.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
+
+
+document.addEventListener('scroll', function() {
+    const headers = document.getElementsByClassName('header');
+    if (headers.length > 0) {
+        const header = headers[0];
+        if (window.scrollY > 100) {  // Adjust scroll position as needed
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    }
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+   
+});
+
